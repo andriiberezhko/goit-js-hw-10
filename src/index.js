960619e4935d85eb68869e05fd1afd0c2c93d381
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-18 19:41:22 +0300

    start
    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+// Додатковий імпорт стилів
+import "notiflix/dist/notiflix-3.2.5.min.css";
 
 const DEBOUNCE_DELAY = 300;
+
+
+const refs = {
+    searchForm: document.querySelector('input#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+
+
+
+refs.searchForm.addEventListener('input', debounce(inputSearch, DEBOUNCE_DELAY));
+
+function inputSearch(e) {
+    const search = e.target.value;
+    // fetchCountries(search);
+    fetchCountries(search).then(r => {
+        if (r.length > 10) {
+          return Notiflix.Notify.info("Too many matches found. Please enter a more specific name."); 
+        };
+        if ( 2 < r.length < 10) {
+           console.log('Oll is rith');
+        };
+        if (r.length < 2) {
+            console.log(r.length);
+           return console.log('we finde one country');
+        }
+    });
+}
+
+
+function fetchCountries(name) {
+    const url = `https://restcountries.com/v3/name/${name}?fields=name,capital,population,flags,languages`;
+   return fetch(url).then(r => r.json());
+}
