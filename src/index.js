ff6857d9a44095af8470a62d1a732bead2511e95
Andrii Berezhko <Berezhko7777@ukr.net>
2022-05-20 13:56:46 +0300

    1
    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
+import countryListTpl from './countryListElement.hbs';
+import countryInfoTpl from './countryInfo.hbs';
 // Додатковий імпорт стилів
 import "notiflix/dist/notiflix-3.2.5.min.css";
 
@@ -19,56 +22,43 @@ const refs = {
 refs.searchForm.addEventListener('input', debounce(inputSearch, DEBOUNCE_DELAY));
 
 function inputSearch(e) {
-    const search = e.target.value;
+    const search = e.target.value.trim();
+    if (!search) {
+        clearMarckup();
+        return;
+    };
    
-    fetchCountries(search.trim())
-        .then(r => {
-                if (r.length > 10) {
-                    refs.countryInfo.innerHTML = ' ';
-                    refs.countryList.innerHTML = ' ';
-                    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-                    return null;
-                };
-                 if (2 <= r.length && r.length < 10) {
-                      
-                    for (const country of r) {
-                        console.log(country);
-                        refs.countryInfo.innerHTML = ' ';
-                        refs.countryList.insertAdjacentHTML('afterbegin', makeCountryListElement(country));
-                    };
-                }
-                 else {
-                      
-                    for (const country of r) {
-                        console.log(country);
-                        refs.countryList.innerHTML = ' ';
-                        refs.countryInfo.insertAdjacentHTML('afterbegin', makeContryInfo(country));
-                     };
-                };
+    fetchCountries(search)
+        .then(country => {
+            if (country.length > 10) {
+                clearMarckup()
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+                return;
+            };
+            if (2 <= country.length && country.length <= 10) {
+                clearMarckup()
+                refs.countryList.insertAdjacentHTML('afterbegin', countryListTpl(country));                                      
+            }
+            else {
+                clearMarckup();
+                const countryInfo = country.map(makeContryInfo).join(' ');
+                refs.countryInfo.insertAdjacentHTML('afterbegin', countryInfo);
+                // 
+                // refs.countryInfo.insertAdjacentHTML('afterbegin', countryInfoTpl(country));
+                                      
+            };
         })
         .catch(error => {
-            refs.countryInfo.innerHTML = ' ';
-            refs.countryList.innerHTML = ' ';
-             Notiflix.Notify.failure("Oops, there is no country with that name");
-        } );
+            console.log("Oops, there is no country with that name");
+            return Notiflix.Notify.failure("Oops, there is no country with that name");
+        });
+        
 };
    
 
- function fetchCountries(name) {
-        const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-        return fetch(url)
-            .then(r => {
-                return r.json()
-            })
-            
-};
 
-function makeCountryListElement({flags, name }) {
-    return `<li class = 'country-list__item'>
-    <img src = '${flags.svg}' alt = '${name.official} flag' width="60px" heigth = '30px'>
-    <p class = 'item__text'>${name.official}</p>
-    </li>`
-};
+
+
 
 function makeContryInfo({ name, capital, flags, population, languages }) {
     return `<img src = '${flags.svg}' alt = '${name.official} flag' width="60px" heigth = '30px'>
@@ -76,5 +66,10 @@ function makeContryInfo({ name, capital, flags, population, languages }) {
     <p>Capital: ${capital}</p>
     <p>Population: ${population}</p>
     <p>Languages: ${Object.values(languages)}</p>`
+}
+
+function clearMarckup() {
+  refs.countryInfo.innerHTML = ' ';
+  refs.countryList.innerHTML = ' ';   
 }
 
